03893c8e94f33479a7e6270d1c9fdc0730d36e57
Filipe Andrade <carlos.filipe.developer@gmail.com>
2020-03-13 20:33:55 -0300

    Adicionando estilo css ao index.html

--- php/classe_calculadora.php
+++ php/classe_calculadora.php
@@ -35,7 +35,7 @@
             return $this->altura;
         }
         public function getPeso() {
-             return $this->peso;
+            return $this->peso;
         }
         public function getIdade() {
             return $this->idade;
@@ -56,74 +56,32 @@
 
         
 			// verificando se todos os dados obrigatorios foram preenchidos
-		if (empty($this->getAltura())) {
-            echo "<script> alert('Informe sua altura para a realização do cálculo') </script>";
-            echo "<script> history.back() </script>";
-
-       }
-       elseif (empty($this->getPeso())) {
-            echo "<script>alert(Informe seu peso para a realização do cálculo')</script>";
-            echo "<script> history.back() </script>";
-       }
-       elseif (empty($this->getIdade())) {
-            echo "<script>alert('Informe sua idade para a realização do cálculo')</script>";
-            echo "<script> history.back() </script>";
-       }
-
-       
-
-
-       else{
-            if ($this->getSexo() == 'masc') {
-                        
-                $this->setTmb(66 + (13.7 * $this->getPeso()) + (5.0 * $this->getAltura()) - (6.8 * $this->getIdade()));
-
-                echo $this->getTmb();
+            if (empty($this->getAltura())) {
+                echo "<script> alert('Informe sua altura para a realização do cálculo') </script>";
+                echo "<script> history.back() </script>";
             }
-            if ($this->getSexo() == 'fem') {
-
-                $this->setTmb(665 + (9.6 *  $this->getPeso()) + (1.8 * $this->getAltura()) - (4.7 * $this->getIdade()));
-                echo $this->getTmb();
+            elseif (empty($this->getPeso())) {
+                echo "<script>alert(Informe seu peso para a realização do cálculo')</script>";
+                echo "<script> history.back() </script>";
             }
-            
-               
-           }
+            elseif (empty($this->getIdade())) {
+                echo "<script>alert('Informe sua idade para a realização do cálculo')</script>";
+                echo "<script> history.back() </script>";
+            }
+            else{
+                if ($this->getSexo() == 'masc') {
                             
+                    $this->setTmb(66 + (13.7 * $this->getPeso()) + (5.0 * $this->getAltura()) - (6.8 * $this->getIdade()));
 
+                    echo $this->getTmb();
+                }
+                if ($this->getSexo() == 'fem') {
 
+                    $this->setTmb(665 + (9.6 *  $this->getPeso()) + (1.8 * $this->getAltura()) - (4.7 * $this->getIdade()));
+                    echo $this->getTmb();
+                }
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-               
-            
-
-               
-                
-            
-        
-     
 
 
         }
