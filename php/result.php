82ca1ed085b310d1bb6271f6add881b17b799aef
Filipe Andrade <carlos.filipe.developer@gmail.com>
2020-03-19 20:35:35 -0300

    correção no front end da pagina result

--- php/result.php
+++ php/result.php
@@ -84,8 +84,8 @@
 			 Copyright &copy; 2020 - by Filipe Andrade	
 		</div>
 		<div class="container d-flex justify-content-center">
-		 	<img src="imagens/face_logo.png" class="img-fluid">	Facebook |
-		 	<img src="imagens/insta_logo.png" class="img-fluid"> Instagram
+		<a href="https://www.linkedin.com/in/carlos-filipe-1232571a0/" target="_blank">	<img src="../imagens/linkedin.png" class="img-fluid">	LinkedIn &nbsp; </a>
+		 <a href="https://www.instagram.com/filipe.andrade_/" target="_blank"> <img src="../imagens/instagram.png" class="img-fluid">	Instagram  </a>
 		</div> 				
 	</footer>
 </body>
